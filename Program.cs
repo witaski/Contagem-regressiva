9d1afe7d613822e490b8a0fb7fc125eef995bac6
Wellington Witaski <wellingtonwitaski@hotmail.com>
2021-08-23 16:13:17 -0300

    Exercíco 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Contagem_regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
